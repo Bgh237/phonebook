294c496493e6040eb2b5beeaf1783a0bb679f81e
Bgh237 <58211837+Bgh237@users.noreply.github.com>
2020-10-28 13:22:15 +0200

    update for build

---
+++ build/precache-manifest.de38a176e5cf5b67a0777c53cdca8140.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534a2548865aa44709f6cc75065f2a4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c48fe6f115d8af53a5f",
+    "url": "/static/css/main.0148a633.chunk.css"
+  },
+  {
+    "revision": "638ef986ed80be7d2ba3",
+    "url": "/static/js/2.26de4d96.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.26de4d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c48fe6f115d8af53a5f",
+    "url": "/static/js/main.a03958c1.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
