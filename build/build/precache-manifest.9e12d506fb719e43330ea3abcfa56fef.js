e9e4faa179570921c1e92b5ac3a9cb3a7e1726d9
Bgh237 <58211837+Bgh237@users.noreply.github.com>
2020-10-28 16:08:02 +0200

    frontend CORS update

---
+++ build/build/precache-manifest.9e12d506fb719e43330ea3abcfa56fef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "539d565fe664394836bab5b1851f956b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16858529f9cea469183a",
+    "url": "/static/css/main.0148a633.chunk.css"
+  },
+  {
+    "revision": "638ef986ed80be7d2ba3",
+    "url": "/static/js/2.26de4d96.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.26de4d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16858529f9cea469183a",
+    "url": "/static/js/main.0fb5c77e.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
